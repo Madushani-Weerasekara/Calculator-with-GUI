0626aa3c79f5b7101fa4b13be36c17e290b4cba4
Madushani <mmweerasekara@gmail.com>
2023-02-16 06:44:49 +0530

    The code defines a function button_click that is called when a number button is clicked in the calculator GUI. The function retrieves the current value displayed in the calculator (current), clears the display, concatenates the current value with the number clicked by the user (number), and inserts the resulting string into the display.
    User input handler function for calculator number button.

--- Calculator with GUI.py
+++ Calculator with GUI.py
@@ -1,14 +1,22 @@
+
 from tkinter import *# import tkinter
 
-root = Tk() # create a new tkinter window using TK() class
+root_window = Tk() # create a new tkinter window using TK() class
 
-root.title("Calculator") # set the title of the window
+root_window.title("Calculator") # set the title of the window
 
-root.mainloop() # display the window. This loop helps to keep the window open untill the user close it or exit the program.
+root_window.mainloop() # display the window. This loop helps to keep the window open untill the user close it or exit the program.
 
-input_box = Entry(root, width=50, borderwidth=5) # create a new entry widget and store it in input_box vsriable
+input_box = Entry(root_window, width=50, borderwidth=5) # create a new entry widget and store it in input_box vsriable
 input_box.grid(row=0, column=0, columnspan=4, padx=10, pady=10) #places the input_box widget inside the root window using the grid() method.
 
+def button_click(number):
+
+    current = input_box.get() #retrieves the current value of the calculator display widget, which is stored in the e variable.
+    input_box.delete(0,END) # clear all -delete all of the text in the widget, starting from the beginning (index 0) and continuing to the end.
+    input_box.insert = str(current) + str(number) # inserts the new number clicked by the user into the display widget by converting it to a string and concatenating it with the previous value stored in current.
+
+ 
 
 
 
